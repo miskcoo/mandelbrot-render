c6a4de12e923952dd3de5ceecb5c0faea44a9e6d
miskcoo <miskcoo@gmail.com>
2016-10-15 22:37:32 +0800

    Optimize iteration speed for small precision.

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -30,6 +30,67 @@ namespace mandelbrot
 		mpfr_init_set(c_real, ri.x0, MPFR_RNDN);
 		mpfr_init_set(c_imag, ri.y0, MPFR_RNDN);
 
+		auto iterate_mpfr = [&]() -> int
+		{
+			// initialize 
+			mpfr_set_ui(Im,  0, MPFR_RNDN);
+			mpfr_set_ui(Re,  0, MPFR_RNDN);
+			mpfr_set_ui(Im2, 0, MPFR_RNDN);
+			mpfr_set_ui(Re2, 0, MPFR_RNDN);
+
+			int iter = 1;
+			for(; iter <= ri.max_iter; ++iter)
+			{
+				// z_{n + 1} = z_n^2 + c;
+
+				// calculate the imagine part of z
+				mpfr_mul      (Im, Im, Re,     MPFR_RNDN);
+				mpfr_mul_2exp (Im, Im, 1,      MPFR_RNDN);
+				mpfr_add      (Im, Im, c_imag, MPFR_RNDN);
+
+				// calculate  the real part of z
+				mpfr_sub      (Re, Re2, Im2,   MPFR_RNDN);
+				mpfr_add      (Re, Re, c_real, MPFR_RNDN);
+
+				// calculate  the square
+				mpfr_mul      (Re2, Re, Re,    MPFR_RNDN);
+				mpfr_mul      (Im2, Im, Im,    MPFR_RNDN);
+
+				// check for exit
+				mpfr_add      (T, Re2, Im2,    MPFR_RNDN);
+
+				if(mpfr_cmp_ui(T, 4) > 0) 
+					break;
+			}
+
+			return iter;
+		};
+
+		auto iterate_double = [&]() -> int
+		{
+			double im = 0, re = 0, im2 = 0, re2 = 0;
+			double c_im = mpfr_get_d(c_imag, MPFR_RNDN);
+			double c_re = mpfr_get_d(c_real, MPFR_RNDN);
+
+			int iter = 1;
+			for(; iter <= ri.max_iter; ++iter)
+			{
+				im  = 2.0 * im * re + c_im;
+				re  = re2 - im2     + c_re;
+				re2 = re * re;
+				im2 = im * im;
+
+				if(re2 + im2 > 4.0)
+					break;
+			}
+
+			mpfr_set_d(Im2, im2, MPFR_RNDN);
+			mpfr_set_d(Re2, re2, MPFR_RNDN);
+			mpfr_set_d(T, re2 + im2, MPFR_RNDN);
+
+			return iter;
+		};
+
 		for(int i = 0; i != ri.height; ++i)
 		{
 			if(control && !control(i)) 
@@ -40,37 +101,7 @@ namespace mandelbrot
 
 			for(int j = 0; j != ri.width; ++j)
 			{
-				// initialize 
-				mpfr_set_ui(Im,  0, MPFR_RNDN);
-				mpfr_set_ui(Re,  0, MPFR_RNDN);
-				mpfr_set_ui(Im2, 0, MPFR_RNDN);
-				mpfr_set_ui(Re2, 0, MPFR_RNDN);
-
-				int iter = 1;
-				for(; iter <= ri.max_iter; ++iter)
-				{
-					// z_{n + 1} = z_n^2 + c;
-
-					// calculate the imagine part of z
-					mpfr_mul      (Im, Im, Re,     MPFR_RNDN);
-					mpfr_mul_2exp (Im, Im, 1,      MPFR_RNDN);
-					mpfr_add      (Im, Im, c_imag, MPFR_RNDN);
-
-					// calculate  the real part of z
-					mpfr_sub      (Re, Re2, Im2,   MPFR_RNDN);
-					mpfr_add      (Re, Re, c_real, MPFR_RNDN);
-
-					// calculate  the square
-					mpfr_mul      (Re2, Re, Re,    MPFR_RNDN);
-					mpfr_mul      (Im2, Im, Im,    MPFR_RNDN);
-
-					// check for exit
-					mpfr_add      (T, Re2, Im2,    MPFR_RNDN);
-
-					if(mpfr_cmp_ui(T, 4) > 0) 
-						break;
-				}
-
+				int iter = ri.prec < 50 ? iterate_double() : iterate_mpfr();
 				ri.buffer[ i * ri.width + j ] = color_picker(iter, ri.max_iter, Re2, Im2, T);
 
 				// calculate next parameter c
